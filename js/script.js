8d85a17262ceb112d45b60e8f44cbd58188892a1
luisnannini <luisnannini97@gmail.com>
2021-10-21 23:38:40 -0300

    add modal and // alerts

--- js/script.js
+++ js/script.js
@@ -243,6 +243,16 @@ function dniOnFocus() {
 dniInput.addEventListener('blur', dniOnBlur)
 dniInput.addEventListener('focus', dniOnFocus)
 
+// Autocomplete Name
+function nameGreet() {
+    var text = nameInput.value
+    var titleName = document.getElementById('name-greet')
+    titleName.style.opacity = '1'
+    titleName.innerHTML = 'Hello ' + text
+}
+nameInput.addEventListener('keydown', nameGreet)
+nameInput.addEventListener('keyup', nameGreet)
+
 // Button
 var button = document.getElementById('button')
 function clickButton() {
@@ -258,19 +268,28 @@ function clickButton() {
         postalOnBlur() &&
         dniOnBlur()
         ) {
-        alert('Your Data: \n' + inputValues.join(' '))
+   //     alert('Your Data: \n' + inputValues.join(' '))
     } else {
-        alert('Error: \n' + inputErrors.join(' '))
+   //     alert('Error: \n' + inputErrors.join(' '))
     }
 }
 button.addEventListener('click', clickButton)
 
-// Autocomplete Name
-function nameGreet() {
-    var text = nameInput.value
-    var titleName = document.getElementById('name-greet')
-    titleName.style.opacity = '1'
-    titleName.innerHTML = 'Hello ' + text
+///////////////////////////////////////////////////Semana 06////////////////////////////////////////////////////////////
+// Get the modal & Span (X)
+var modal = document.getElementById('myModal')
+var span = document.getElementsByClassName('close')[0]
+// When the user clicks on the button, open the modal
+button.onclick = function() {
+  modal.style.display = 'block'
 }
-nameInput.addEventListener('keydown', nameGreet)
-nameInput.addEventListener('keyup', nameGreet)
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = 'none'
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none'
+  }
+}
